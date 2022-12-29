048b1d9274be09fa30a3275d6acd89ab0b858332
YakshaRN <yaksharamnandan@gmail.com>
2022-12-29 06:03:07 +0530

    implemented flip in profile cards

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -4,12 +4,21 @@ import './ProfileCard.css';
 
 const ProfileCard = () => {
     return (
-        <div className="container">
-            <img src={dp}/>
-            <h2>YAKSHA RAM NANDAN</h2>
-            <h3>Full-Stack Developer</h3>
-            <h4>Ex- BharatAgri, Bangalore</h4>
-            <h4>Ex- CitiBank, Pune</h4>
+        <div className="main-container">
+            <div className="container">
+                <div className="front-card">
+                    <img src={dp}/>
+                    <h2>YAKSHA RAM NANDAN</h2>
+                    <h3>Full-Stack Developer</h3>
+                    <h4>Ex- BharatAgri, Bangalore</h4>
+                    <h4>Ex- CitiBank, Pune</h4>
+                </div>
+                <div className="back-card">
+                    <img src={dp}/>
+                    <h4>8229048516</h4>
+                    <h4>yaksharamnandan@gmail.com</h4>
+                </div>
+            </div>
         </div>
     );
 }
